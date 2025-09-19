7fb7df9caec314c7a0c7490fdb7166a017563474
iceyfisher <wxj-123@shu.edu.cn>
2025-09-19 15:10:30 +0800

    Site updated: 2025-09-19 15:10:16

---
+++ js/binft-typewriter.js
@@ -0,0 +1,58 @@
+var binft = function (r) {
+    var isTransparent = true;
+    function getRandomColor() {
+        if(isTransparent){
+            isTransparent = false;
+            //此处修改字体颜色,最后的 0 和 1 不要改
+            return "rgba(255,255,255,0)"
+        }else{
+            isTransparent = true;
+            return "rgba(255,255,255,1)"
+        }
+    }  
+    function n(r) {
+        for (var n = document.createDocumentFragment(), i = 0; r > i; i++) {
+            var oneword = document.createElement("span");
+            oneword.textContent = "_"; // 此处是末尾字符,如果想用光标样式可以改为"|"
+            oneword.style.color = getRandomColor();
+            n.appendChild(oneword);
+        }
+        return n
+    }
+    function i() {
+        var t = wordList[c.skillI];
+        c.step ? c.step-- : (c.step = refreshDelayTime, c.prefixP < l.length ? (c.prefixP >= 0 && (c.text += l[c.prefixP]), c.prefixP++) : "forward" === c.direction ? c.skillP < t.length ? (c.text += t[c.skillP], c.skillP++) : c.delay ? c.delay-- : (c.direction = "backward", c.delay = showTotalWordDelayTime) : c.skillP > 0 ? (c.text = c.text.slice(0, -1), c.skillP--) : (c.skillI = (c.skillI + 1) % wordList.length, c.direction = "forward")), r.textContent = c.text, r.appendChild(n(c.prefixP < l.length ? Math.min(maxLength, maxLength + c.prefixP) : Math.min(maxLength, t.length - c.skillP))), setTimeout(i, d)
+    }
+    var l = "",
+    //此处改成你自己的诗词
+    wordList = [ 
+        "起舞弄清影 何似在人間",
+        "人生如夢 一尊還酹江月",
+        "莫聽穿林打葉聲 何妨吟嘯且徐行",
+        "誰道人生無再少 門前流水尚能西",
+        "枝上柳綿吹又少 天涯何處無芳草",
+        "小舟從此逝 江海寄餘生",
+        "缺月掛疏桐 漏斷人初靜",
+        "休對故人思故國 且將新火試新茶",
+        "但遠山長 雲山亂 曉山青",
+        "夜茫茫 重尋無處 覺來小園行遍",
+        "待浮花 浪蕊都盡 伴君幽獨",
+        ].map(function (r) {
+    return r + ""
+    }),
+    showTotalWordDelayTime = 2,
+    refreshDelayTime = 1,
+    maxLength = 1,
+    d = 75,
+    c = {
+        text: "",
+        prefixP: -maxLength,
+        skillI: 0,
+        skillP: 0,
+        direction: "forward",
+        delay: showTotalWordDelayTime,
+        step: refreshDelayTime
+    };
+    i()
+};
+binft(document.getElementById('binft'));
